181f4397cccfab11303ce64262815751e740ff6f
HaVinh <tranhavinh2003@gmail.com>
2025-04-26 16:05:45 +0700

    Updates

---
+++ precache-manifest.6811dcc0cfdfeb858c9ec7fec89acfb1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed88d4a06cadeb7ce489c0d0a3931f6",
+    "url": "/Bio/index.html"
+  },
+  {
+    "revision": "11ac03de2d1c9fcd623c",
+    "url": "/Bio/static/css/main.a39f7dc4.chunk.css"
+  },
+  {
+    "revision": "d950a244a0e9101411f6",
+    "url": "/Bio/static/js/2.67b84e4e.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/Bio/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "11ac03de2d1c9fcd623c",
+    "url": "/Bio/static/js/main.6bcc8407.chunk.js"
+  },
+  {
+    "revision": "75f63a608485fc6981b4",
+    "url": "/Bio/static/js/runtime~main.503c7467.js"
+  },
+  {
+    "revision": "8d1c0d17185341256e31e93dbf207e61",
+    "url": "/Bio/static/media/Qr.8d1c0d17.jpeg"
+  },
+  {
+    "revision": "f0fdf4066e39d0fef09b744e51fd9b34",
+    "url": "/Bio/static/media/jett-background.f0fdf406.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Bio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "779954f3ba48ae9a01acdbe36011bd14",
+    "url": "/Bio/static/media/meo1.779954f3.jpeg"
+  }
+]);
